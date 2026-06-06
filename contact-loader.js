b77da90fd2302dbd6fbb6769df979d9f5ee81f3d
Joaquín Avilés <48577264+joacoAviles@users.noreply.github.com>
2026-06-05 21:17:58 -0400

    Disable contact fetch loader [allow-contact-change]

--- contact-loader.js
+++ contact-loader.js
@@ -2,23 +2,5 @@
   var mount = document.querySelector('[data-contact-include]');
   if (!mount) return;
 
-  function notifyLoaded() {
-    document.dispatchEvent(new CustomEvent('avia:contact-loaded'));
-  }
-
-  notifyLoaded();
-
-  fetch('sections/contact.html', { headers: { Accept: 'text/html' } })
-    .then(function (response) {
-      if (!response.ok) throw new Error('No se pudo cargar la sección de contacto');
-      return response.text();
-    })
-    .then(function (html) {
-      if (!html || !html.includes('contact-section')) return;
-      mount.innerHTML = html;
-      notifyLoaded();
-    })
-    .catch(function () {
-      notifyLoaded();
-    });
+  document.dispatchEvent(new CustomEvent('avia:contact-loaded'));
 })();
